4df4b93799d79b636e6b172a190c3dca637f2d76
Fazil-SA <fazilsa2000@gmail.com>
2024-02-23 00:59:04 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -1,12 +1,16 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import userRouter from './routes/user.route.js'
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 const app = express();
 
+app.use(express.json());
+
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
 
 app.listen(3000, () => {
     console.log("Served started listening 3000")
