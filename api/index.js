e2f15e78beed1bd8f7387b6501f52c9c493dbe04
Fazil-SA <fazilsa2000@gmail.com>
2024-02-22 23:50:18 +0530

    Database Connected

--- api/index.js
+++ api/index.js
@@ -1,5 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 
-app.listen(3000, () => console.log("Served started listening 3000"));
+app.listen(3000, () => {
+    console.log("Served started listening 3000")
+    mongoose.connect(process.env.MONGO).then(() => {
+        console.log("Connected to Database");
+    }).catch((error) => {
+        console.log(error);
+    })
+});
