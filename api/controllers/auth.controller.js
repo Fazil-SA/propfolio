73414090e1a193d2cccb822b9b9a128c5529442f
Fazil-SA <fazilsa2000@gmail.com>
2024-02-25 10:24:42 +0530

    signin api wrapped in trycatch block

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -18,12 +18,16 @@ export const signup = async (req,res,next) => {
 
 export const signin = async (req,res,next) => {
     const {email, password} = req.body;
-    const validUser = await User.findOne({email});
-    if(!validUser) return next(errorHandler(404, 'User not found!'));
-    const validPassword = bcryptjs.compareSync(password, validUser.password);
-    if(!validPassword) return next(errorHandler(401, 'Wrong Password!'));
-    const {password: pass, ...userWithoutPassword} = validUser.toObject();
-    console.log("userWithoutPassword", userWithoutPassword);
-    const token = jwt.sign({ id:validUser._id }, process.env.JWT_SECRET);
-    res.cookie('access_token' ,token, {httpOnly: true}).status(200).json(userWithoutPassword);
+    try {
+        const validUser = await User.findOne({email});
+        if(!validUser) return next(errorHandler(404, 'User not found!'));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if(!validPassword) return next(errorHandler(401, 'Wrong Password!'));
+        const {password: pass, ...userWithoutPassword} = validUser.toObject();
+        console.log("userWithoutPassword", userWithoutPassword);
+        const token = jwt.sign({ id:validUser._id }, process.env.JWT_SECRET);
+        res.cookie('access_token' ,token, {httpOnly: true}).status(200).json(userWithoutPassword);
+    } catch (error) {
+        next(error);        
+    }
 }
