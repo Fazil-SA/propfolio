d1d92dfed47343f5e8862fbdf9a3685d119726fc
Fazil-SA <fazilsa2000@gmail.com>
2024-02-29 13:11:24 +0530

    listing page completed

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -5,6 +5,14 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore from "swiper";
 import { Navigation } from "swiper/modules";
 import "swiper/css/bundle";
+import {
+  FaBath,
+  FaBed,
+  FaChair,
+  FaMapMarkerAlt,
+  FaParking,
+  FaShare,
+} from 'react-icons/fa';
 
 function Listing() {
   SwiperCore.use([Navigation]);
@@ -45,6 +53,46 @@ function Listing() {
                   </SwiperSlide>
                 ))}
               </Swiper>
+            <div className="max-w-4xl mx-auto my-5 p-3 w-full">
+                  <p className="text-xl font-semibold">{listingData.title} - ₹{' '} {listingData.offer ? <>
+                    <span className="line-through text-slate-700">{listingData.regularPrice.toLocaleString('en-IN')} {' '}</span>
+                    <span className="ml-2">{listingData.discountedPrice.toLocaleString('en-IN')}</span>
+                  </> 
+                     : listingData.regularPrice.toLocaleString('en-US')}{listingData.type === "rent" && '/month'}
+                  </p>
+                  <p className="flex text-sm text-slate-600 items-center gap-2 my-1"><FaMapMarkerAlt className="text-green-700" /> {listingData.address}</p>
+                  <div className="my-4 flex gap-2">
+                    <p className="bg-red-800 font-normal rounded-lg w-full flex justify-center text-white p-2">{listingData.type === "rent" ? 'For Sale' : 'For Sale'}</p>
+                    {
+                      listingData.discountedPrice > 1 && (
+                        <p className="bg-green-800 font-normal flex rounded-lg w-full justify-center text-white p-2">₹ {+listingData.regularPrice - +listingData.discountedPrice} discount</p>
+                      )
+                    }
+                  </div>
+                  <p><span className="font-semibold">Details: </span>{listingData.description}</p>
+                  <div className="flex gap-2 flex-wrap mt-4 text-green-900">
+                    {listingData.bedrooms && (
+                      <>
+                        <p className="flex items-center">
+                          <FaBed />
+                          <span className="p-2">{listingData.bedrooms} Beds</span>
+                        </p>
+                        <p className="flex items-center">
+                          <FaBath />
+                          <span className="p-2">{listingData.bathrooms} Baths</span>
+                        </p>
+                        <p className="flex items-center">
+                          <FaParking />
+                          <span className="p-2">{listingData.parking ? 'Parking' : 'No Parking'} </span>
+                        </p>
+                        <p className="flex items-center">
+                          <FaChair />
+                          <span className="p-2">{listingData.furnished ? 'Furnished' : 'Not Furnished'} Beds</span>
+                        </p>
+                      </>
+                    )}
+                  </div>
+            </div>
             </div>
         )}
     </main>
